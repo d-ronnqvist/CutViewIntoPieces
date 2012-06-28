3fa65387661041b933c071cea4f6e2d269c1b6a4
David Rönnqvist <david.ronnqvist@gmail.com>
2012-06-28 11:25:26 +0200

    Initial upload to GitHub
    At this points the content is drawn to an image that in turn is drawn
    to more images and put into many different views that animate the
    anchorPoint infinitely.

--- CutViewIntoPieces/DRViewController.m
+++ CutViewIntoPieces/DRViewController.m
@@ -21,35 +21,79 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(50, 50, 320-100, 200)];
-    [self.view addSubview:textView];
+//    [self.view addSubview:textView];
     [textView setText:@"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis consequat adipiscing arcu, porttitor feugiat lectus ullamcorper quis. Donec et sem tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean nec nisl sit amet risus convallis dapibus. Nunc suscipit massa in enim facilisis vehicula. Nulla porta lectus ut metus lobortis tempor. Nam ornare elit vel ante blandit tincidunt. Suspendisse justo diam, convallis eleifend fermentum quis, aliquet et odio. Nam vehicula porttitor tincidunt. Sed porttitor, sem ut posuere adipiscing, nisl metus scelerisque neque, in lacinia lectus mauris ac mi. Etiam laoreet pretium cursus. Nunc placerat vehicula ligula ut tempus. Cras et faucibus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam eu felis a risus blandit dictum. Nulla justo nisl, posuere eu lacinia ac, iaculis in diam."];
     [textView setBackgroundColor:[UIColor orangeColor]];
     [textView setFont:[UIFont fontWithName:@"Noteworthy-Bold" size:14]];
     
+    UIView *tornView = [[UIView alloc] initWithFrame:[textView frame]];
+    tornView.layer.anchorPoint = CGPointMake(0.5, 0.0);
+    tornView.layer.position = CGPointMake(CGRectGetMidX([textView frame]),
+                                          CGRectGetMaxY([textView frame]));
     
+    [self.view addSubview:tornView];
     
     UIImage *splitMeInTwo = [self imageRepresentationOfView:textView];
     
-    // The size of each part is half the height of the whole image
-    CGSize size = CGSizeMake([splitMeInTwo size].width, [splitMeInTwo size].height/2);
+    NSUInteger numberOfImages = 10;
+    NSMutableArray *imageViews = [NSMutableArray arrayWithCapacity:numberOfImages];
     
-    // Create image-based graphics context for top half
-    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
+    for (NSUInteger imageIndex = 0 ; imageIndex < numberOfImages ; imageIndex++ ) {
+        // The size of each part is half the height of the whole image
+        CGSize size = CGSizeMake([splitMeInTwo size].width/numberOfImages,
+                                 [splitMeInTwo size].height);
+        
+        // Create image-based graphics context for top half
+        UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
+        
+        // Draw into context, bottom half is cropped off
+        //[image drawAtPoint:CGPointMake(0.0,0.0)];
+        [splitMeInTwo drawAtPoint:CGPointMake(-size.width*imageIndex,0.0) 
+                        blendMode:kCGBlendModeNormal 
+                            alpha:1.0];
+        
+        // Grab the current contents of the context as a UIImage 
+        // and add it to our array
+        UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+        imageView.layer.anchorPoint = CGPointMake(0.5, 0.0);
+        imageView.frame = CGRectMake(size.width*imageIndex, 0, size.width, size.height);
+        imageView.layer.shadowPath = CGPathCreateWithRect(imageView.bounds, NULL);
+        imageView.layer.shadowColor = [UIColor blackColor].CGColor;
+        imageView.layer.shadowOffset = CGSizeMake(0, 1);
+        imageView.layer.shadowOpacity = 0.75;
+        imageView.layer.shadowRadius = 2.0;
+        
+        [tornView addSubview:imageView];
+        [imageViews addObject:imageView];
+        
+        CGFloat smallAngle = M_PI/45;
+        CGFloat randomSeed = rand()%10-5.0;
+        imageView.layer.transform = CATransform3DMakeRotation(smallAngle * randomSeed, 1.0, 0.0, 0.0);
+        
+        UIGraphicsEndImageContext();
+    }
     
-    // Draw into context, bottom half is cropped off
-    //[image drawAtPoint:CGPointMake(0.0,0.0)];
-    [splitMeInTwo drawAtPoint:CGPointMake(0.0,0.0) blendMode:kCGBlendModeNormal alpha:1.0];
     
-    // Grab the current contents of the context as a UIImage 
-    // and add it to our array
-    UIImage *top = UIGraphicsGetImageFromCurrentImageContext();
-    UIImageView *topV = [[UIImageView alloc] initWithImage:top];
-    topV.frame = CGRectMake(0, 0, size.width, size.height);
-
-    [self.view addSubview:topV];
-
+    CATransform3D perspeciveTransform = CATransform3DIdentity;
+    perspeciveTransform.m34 = -1.0/900.0;
+    perspeciveTransform = CATransform3DTranslate(perspeciveTransform, 30, 30, 0);
     
-    UIGraphicsEndImageContext();
+    
+    tornView.layer.sublayerTransform = perspeciveTransform;
+    
+    
+    [imageViews enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        UIImageView *imageView = obj;
+        CABasicAnimation *moveFromAnchor = [CABasicAnimation animationWithKeyPath:@"anchorPoint"];
+        [moveFromAnchor setToValue:[NSValue valueWithCGPoint:CGPointMake(0.5, 1.0)]];
+        [moveFromAnchor setDuration:1.5];
+        [moveFromAnchor setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut]];
+        [moveFromAnchor setAutoreverses:YES];
+        [moveFromAnchor setRepeatCount:INFINITY];
+        
+        [imageView.layer addAnimation:moveFromAnchor forKey:@"move"];
+    }];
 }
 
 - (UIImage *)imageRepresentationOfView:(UIView *)view {
